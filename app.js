dfdb9e6371ef7067f5331cc909e5b0b295eafcb8
Chocolatiu <luis_fciencias@ciencias.unam.mx>
2025-08-08 13:35:04 -0600

    muestra la lista de amigos en la pàgina

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
-    // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-    let amigos = [];
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  const amigoIngresado = document.getElementById("amigo").value.trim();
+  if (amigoIngresado !== "") {
+    amigos.push(amigoIngresado);
+  } else {
+    inyectarTextoElemento("#resultado", `Ingresa un nombre vàlido.`);
+  }
+  console.log(amigos);
+  console.log(amigoIngresado);
+  limpiarCampo();
+  listaAmigos();
+}
+
+function listaAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = ""; // Limpiar la lista antes de mostrar los amigos
+  for (let i = 0; i < amigos.length; i++){
+    const li = document.createElement("li");
+    li.textContent = amigos[i]; 
+    lista.appendChild(li);
+    
+  }
+  
+}
 
 
- function agregarAmigo() {
-        const amigoIngresado = document.getElementById("amigo").value.trim();
-        if (amigoIngresado !== "") {
-            amigos.push(amigoIngresado);
-        } else {
-            inyectarTextoElemento('#resultado', `Ingresa un nombre vàlido.`);
-        }   
-        console.log(amigos);
-        console.log(amigoIngresado);
-        limpiarCampo();
-        
-    }
 
- 
+function amigoSorteado() {
+  const randomIndex = Math.floor(Math.random() * amigos.length);
+  const amigoSeleccionado = amigos[randomIndex];
 
+  if (amigos.length > 0) {
+    amigos.splice(randomIndex, 1); // Eliminar el amigo seleccionado de la lista
+    inyectarTextoElemento(
+      "#resultado",
+      `Tu amigo secreto es: ${amigoSeleccionado}`
+    );
+  } else {
+    inyectarTextoElemento("#resultado", `No hay amigos para seleccionar.`);
+  }
 
-
-    function amigoSorteado() {
-        const randomIndex = Math.floor(Math.random() * amigos.length);
-        const amigoSeleccionado = amigos[randomIndex];
-
-        if (amigos.length > 0) { 
-        amigos.splice(randomIndex, 1); // Eliminar el amigo seleccionado de la lista
-        inyectarTextoElemento('#resultado', `Tu amigo secreto es: ${amigoSeleccionado}`);
-        }else {
-        inyectarTextoElemento('#resultado', `No hay amigos para seleccionar.`);
-        }
-
-    /* if (!amigos.includes(amigoSeleccionado)) {
+  /* if (!amigos.includes(amigoSeleccionado)) {
             console.log(amigoSeleccionado);
             inyectarTextoElemento('#resultado', `Tu amigo secreto es: ${amigoSeleccionado}`);
             return;  
@@ -38,22 +50,19 @@
             amigoSorteado();
             return;
         }*/
-    }
-  
+}
 
+function limpiarCampo() {
+  document.querySelector("#amigo").value = "";
+  return;
+}
 
-
-    function limpiarCampo(){
-            document.querySelector('#amigo').value = "";
-            return;
-        }
-
-    function inyectarTextoElemento(elemento, texto){
-            let elementoHTML = document.querySelector(elemento);
-            elementoHTML.innerHTML = texto;
-            return;  
-        }
-        agregarAmigo();
-        amigoSorteado();
-
-        
+function inyectarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+  return;
+}
+agregarAmigo();
+amigoSorteado();
+listaAmigos();
+// Aquí puedes agregar más funciones o lógica según sea necesario.
