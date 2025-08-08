e3452c53826485d8a1bcee4b538ab8c325846fcd
Chocolatiu <luis_fciencias@ciencias.unam.mx>
2025-08-07 18:46:24 -0600

    alcanza funcionalidad casi  completa

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+    // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+    let amigos = [];
+
+
+ function agregarAmigo() {
+        const amigoIngresado = document.getElementById("amigo").value.trim();
+        if (amigoIngresado !== "") {
+            amigos.push(amigoIngresado);
+        } else {
+            inyectarTextoElemento('#resultado', `Ingresa un nombre vàlido.`);
+        }   
+        console.log(amigos);
+        console.log(amigoIngresado);
+        limpiarCampo();
+        
+    }
+
+ 
 
 
 
-function amigoSorteado() {
-    const randomIndex = Math.floor(Math.random() * amigos.length);
-    const amigoSeleccionado = amigos[randomIndex];
-    console.log(amigoSeleccionado);
-    inyectarTextoElemento('#resultado', `Tu amigo secreto es: ${amigoSeleccionado}`);
+    function amigoSorteado() {
+        const randomIndex = Math.floor(Math.random() * amigos.length);
+        const amigoSeleccionado = amigos[randomIndex];
 
-
-   /* if (!amigos.includes(amigoSeleccionado)) {
-        console.log(amigoSeleccionado);
+        if (amigos.length > 0) { 
+        amigos.splice(randomIndex, 1); // Eliminar el amigo seleccionado de la lista
         inyectarTextoElemento('#resultado', `Tu amigo secreto es: ${amigoSeleccionado}`);
-        return;  
-    } else {
+        }else {
+        inyectarTextoElemento('#resultado', `No hay más amigos para seleccionar.`);
+        }
+
+    /* if (!amigos.includes(amigoSeleccionado)) {
+            console.log(amigoSeleccionado);
+            inyectarTextoElemento('#resultado', `Tu amigo secreto es: ${amigoSeleccionado}`);
+            return;  
+        } else {
+            amigoSorteado();
+            return;
+        }*/
+    }
+  
+
+
+
+    function limpiarCampo(){
+            document.querySelector('#amigo').value = "";
+            return;
+        }
+
+    function inyectarTextoElemento(elemento, texto){
+            let elementoHTML = document.querySelector(elemento);
+            elementoHTML.innerHTML = texto;
+            return;  
+        }
+        agregarAmigo();
         amigoSorteado();
-        return;
-    }*/
-}
 
-
-
-function agregarAmigo() {
-    const amigoIngresado = document.getElementById("amigo").value;
-    console.log(amigoIngresado);    
-    amigos.push(amigoIngresado);
-    console.log(amigos); 
-    limpiarCampo();
-    return;
-}
-
-
-
-function limpiarCampo(){
-        document.querySelector('#amigo').value = "";
-        return;
-    }
-
-function inyectarTextoElemento(elemento, texto){
-        let elementoHTML = document.querySelector(elemento);
-        elementoHTML.innerHTML = texto;
-        return;  
-    }
-    agregarAmigo();
-    amigoSorteado();
-
-    
+        
